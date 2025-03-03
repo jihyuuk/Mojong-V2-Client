b3ebf654ab05b5c2016c1ee2d024943127c9f352
jihyuuk_home <jihyuk.dev@gmail.com>
2025-03-04 06:25:50 +0900

    Style : 메인페이지 아이템 리스트 디자인 구현

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,10 @@
-import { Button, Nav } from 'react-bootstrap';
+import { Button, ListGroup, Nav } from 'react-bootstrap';
 import React from 'react';
 
 function MainPage() {
+
+    const numbers = Array.from({ length: 10 }, (_, i) => i + 1); // 1~10까지 생성
+
     return (
         // 메인페이지
         <div className="MainPage d-flex flex-column h-100 text-center">
@@ -11,13 +14,13 @@ function MainPage() {
 
                 {/* 로고 */}
                 <div className='pt-1'>
-                    <img src="/로고2.png" alt="그린아그로 로고"  style={{maxWidth:"100px"}}/>
+                    <img src="/로고2.png" alt="그린아그로 로고" style={{ maxWidth: "100px" }} />
                 </div>
 
                 {/* 카테고리 */}
                 <div className='overflow-auto p-2'>
                     <Nav variant="underline" className="d-flex flex-nowrap text-nowrap" defaultActiveKey="link-1">
-                        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+                        {numbers.map((num) => (
                             <Nav.Item key={num}>
                                 <Nav.Link className='custom-nav-link' eventKey={`link-${num}`}>Option {num}</Nav.Link>
                             </Nav.Item>
@@ -27,19 +30,29 @@ function MainPage() {
             </div>
 
             {/* 콘텐츠 */}
-            <div className='MainContent border border-black flex-grow-1 overflow-y-auto'>
-                <div className='p-2'>Main content Start</div>
-                <div className='border border-success p-3 mb-3'>item 1</div>
-                <div className='border border-success p-3 mb-3'>item 2</div>
-                <div className='border border-success p-3 mb-3'>item 3</div>
-                <div className='border border-success p-3 mb-3'>item 4</div>
-                <div className='border border-success p-3 mb-3'>item 5</div>
-                <div className='border border-success p-3 mb-3'>item 6</div>
-                <div className='border border-success p-3 mb-3'>item 7</div>
-                <div className='border border-success p-3 mb-3'>item 8</div>
-                <div className='border border-success p-3 mb-3'>item 9</div>
-                <div className='border border-success p-3'>item 10</div>
-                <div className='p-2'>Main content End</div>
+            <div className='flex-grow-1 overflow-y-auto text-start bg-secondary-subtle'>
+                {numbers.map((num) => (
+
+                    <ListGroup className='mb-4 bg-white rounded-0'>
+                        <div className='fs-3 fw-bold p-2 ps-3'>카테고리 {num}</div>
+
+
+                        {numbers.map((num2) => (
+
+                            <ListGroup.Item className='d-flex gap-3'>
+                                <div className='p-4 border rounded-4'>
+                                    사진
+                                </div>
+                                <div>
+                                    <div className="fw-semibold fs-5">리스트 아이템 {num2}</div>
+                                    <div className='mt-1'>5,000원</div>
+                                </div>
+                            </ListGroup.Item>
+
+                        ))}
+                    </ListGroup>
+
+                ))}
             </div>
 
             {/* 푸터 */}
