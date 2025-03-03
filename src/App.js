269f8c0fc1f7930e350dcea785cb29708fcb8a1a
jihyuuk_home <jihyuk.dev@gmail.com>
2025-03-04 05:56:07 +0900

    Style : 메인페이지 헤더 디자인 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import MainPage from './pages/MainPage';
 
 function App() {
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,30 @@
-import { Button } from 'react-bootstrap';
+import { Button, Nav } from 'react-bootstrap';
 import React from 'react';
 
 function MainPage() {
     return (
         // 메인페이지
         <div className="MainPage d-flex flex-column h-100 text-center">
+
             {/* 헤더 */}
-            <div className='MainHeader border border-danger p-4'>MainHeader</div>
+            <div>
+
+                {/* 로고 */}
+                <div className='pt-1'>
+                    <img src="/로고2.png" alt="그린아그로 로고"  style={{maxWidth:"100px"}}/>
+                </div>
+
+                {/* 카테고리 */}
+                <div className='overflow-auto p-2'>
+                    <Nav variant="underline" className="d-flex flex-nowrap text-nowrap" defaultActiveKey="link-1">
+                        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+                            <Nav.Item key={num}>
+                                <Nav.Link className='custom-nav-link' eventKey={`link-${num}`}>Option {num}</Nav.Link>
+                            </Nav.Item>
+                        ))}
+                    </Nav>
+                </div>
+            </div>
 
             {/* 콘텐츠 */}
             <div className='MainContent border border-black flex-grow-1 overflow-y-auto'>
@@ -25,7 +43,7 @@ function MainPage() {
             </div>
 
             {/* 푸터 */}
-            <div className="text-center p-2 pb-3 border-top">
+            <div className="p-2 pb-3 border-top">
                 <Button variant="success" className="w-100 fs-5 fw-semibold p-2 rounded-4">
                     15,000원 <span className='fw-medium'>· 장바구니</span>
                 </Button>
