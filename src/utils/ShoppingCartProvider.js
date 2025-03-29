b854d992f14615d6feefd57afe710723ee4a37e4
jihyuuk_home <jihyuk.dev@gmail.com>
2025-03-29 20:48:52 +0900

    #13 Feat : 장바구니 로컬 스토리지 저장 구현 ( 새로고침, 종료 시에도 장바구니 아이템 유지 가능)

--- src/utils/ShoppingCartProvider.js
+++ src/utils/ShoppingCartProvider.js
@@ -7,7 +7,12 @@ const ShoppingCartContext = createContext();
 export function ShoppingCartProvider({ children }) {
 
     //장바구니에 담긴 아이템들
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState(() => {
+        //로컬스토리지에서 불러오기
+        const savedCart = localStorage.getItem('cartItems');
+        return savedCart ? JSON.parse(savedCart) : [];
+      }); 
+
     const [totalPrice, setTotalPrice] = useState(0);
     const [totalQuantity, setTotalQuantity] = useState(0);
 
@@ -24,6 +29,9 @@ export function ShoppingCartProvider({ children }) {
         //값 업데이트
         setTotalPrice(calPrice);
         setTotalQuantity(calQuantity);
+
+        //로컬 스토리지 저장
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
     }, [cartItems]);
 
     return (
