f8f314f76e9095c7b0b216800128cab273274bdb
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-01 21:54:38 +0900

    Feat : 장바구니 불러오기시에 검증 추가

--- src/utils/ShoppingCartProvider.js
+++ src/utils/ShoppingCartProvider.js
@@ -4,17 +4,38 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ShoppingCartContext = createContext();
 
 //2. Provider 컴포넌트 생성
-export function ShoppingCartProvider({ children }) {
+export function ShoppingCartProvider({ children, menu }) {
 
     //장바구니에 담긴 아이템들
-    const [cartItems, setCartItems] = useState(() => {
-        //로컬스토리지에서 불러오기
-        const savedCart = localStorage.getItem('cartItems');
-        return savedCart ? JSON.parse(savedCart) : [];
-      }); 
+    const [cartItems, setCartItems] = useState([]);
 
     const [totalPrice, setTotalPrice] = useState(0);
     const [totalQuantity, setTotalQuantity] = useState(0);
+
+    //로컬스토리지에서 불러오기
+    const getLocal = () => {
+        //불러오기
+        let savedCart = localStorage.getItem("cartItems");
+        //없으면 return
+        if (!savedCart) return;
+
+        //있으면 검증
+        //menu 돌면서 품절이거나 없는 상품인지 확인
+        savedCart = JSON.parse(savedCart);
+        const updatedCart = savedCart.filter((item) => {
+            // 품절된 항목을 제외하고, 메뉴에 존재하는 항목만 필터링
+            const menuItem = menu.flatMap(category => category.items).find(menuItem => menuItem.id === item.id);
+            return menuItem && menuItem.stock > 0;  // 품절이 아니면
+        });
+
+        //적용하기
+        setCartItems(updatedCart);
+    }
+
+    useEffect(() => {
+        getLocal();
+    }, [menu]);
+
 
     //총 수량, 품목수, 금액 계산 
     useEffect(() => {
