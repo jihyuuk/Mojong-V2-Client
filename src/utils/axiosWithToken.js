be81de6193d40ea843758062e505f4bf6b86b3c6
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-01 21:31:46 +0900

    Feat : 401 에러시에 토큰 재요청하는 인터셉터 추가함

--- src/utils/axiosWithToken.js
+++ src/utils/axiosWithToken.js
@@ -24,5 +24,36 @@ axiosWithToken.interceptors.request.use(
     }
 );
 
+//응답 인터셉터 설정
+axiosWithToken.interceptors.response.use(
+    response => {
+        return response;
+    },
+    async error => {
+        //401에러 즉 토큰 x
+        if (error.response && error.response.status === 401) {
+            try {
+                // 새 토큰을 /guest-token 경로로 요청
+                const response = await axios.get('http://192.168.0.3:8080/guest-token');
+                const newToken = response.headers['authorization'];
+
+                // 새 토큰을 로컬 스토리지에 저장
+                localStorage.setItem('token', newToken);
+
+                // 원래 요청을 새 토큰과 함께 재시도
+                error.config.headers['Authorization'] = newToken;
+
+                // 원래 요청을 새 토큰으로 재전송
+                return axios(error.config);
+            } catch (tokenError) {
+                return Promise.reject(tokenError);
+            }
+        }
+
+        // 401 외의 다른 에러는 그대로 반환
+        return Promise.reject(error);
+    }
+)
+
 export default axiosWithToken;
 
