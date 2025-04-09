e5dbbd7952bff31c3ce10d2f4431176a70ecc6f7
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-10 07:24:53 +0900

    #15  Layout : 상세페이지 레이아웃 키패드로 변경

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import MotionPage from '../motions/MotionPage';
 import Footer from '../components/Footer';
 import { useShoppingCart } from '../utils/ShoppingCartProvider';
+import { Button } from 'react-bootstrap';
 
 function DetailPage() {
 
@@ -36,12 +37,12 @@ function DetailPage() {
         if (!item) return;
 
         //수량 초과 검증하기
-        if(quantity > item.stock){
+        if (quantity > item.stock) {
             setQuantity(item.stock);
-            showTost("재고가 부족합니다. (재고:"+item.stock+"개)")
+            showTost("재고가 부족합니다. (재고:" + item.stock + "개)")
             return;
-        }        
-        
+        }
+
         setTotal(item.price * quantity);
     }, [quantity])
 
@@ -93,6 +94,15 @@ function DetailPage() {
         navigate(-1); // 상세페이지 닫기
     };
 
+    //넘버패드===========================================
+    const numberPad = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9'],
+        ['C', '0', '⌫']
+    ];
+
+    const plusValue = [5, 10, 50, 72];
 
     //상품 클릭 없이 직접 /detail로 접근시 리다이렉트
     useEffect(() => {
@@ -112,60 +122,79 @@ function DetailPage() {
 
                 {/* 메인 */}
                 <main className='flex-grow-1 overflow-y-auto p-3 pt-0'>
-                    {/* 사진 */}
-                    <div className='text-center'>
-                        <img src={item.photo} style={{ maxHeight: '200px' }} />
+                    <div className='d-flex align-items-center w-100 mt-2'>
+                        {/* 이름, 단가 */}
+                        <div className='flex-grow-1 pe-1'>
+                            <div className="fw-semibold fs-4">
+                                {item.name}
+                            </div>
+                            <div className="text-success fw-semibold mt-1">
+                                ₩ {item.price.toLocaleString('ko-KR')}
+                            </div>
+                        </div>
+
+                        {/* 사진 */}
+                        <div>
+                            <img src={item.photo} className='rounded-3' style={{ width: '5rem' }} />
+                        </div>
                     </div>
 
-                    <div className='fw-bold fs-1'>{item.name}</div>
-                    <div className='py-3 text-secondary'>{item.description}</div>
-
-                    <div className='mt-3 fw-semibold fs-5 d-flex justify-content-between'>
-                        <div>
-                            가격
-                        </div>
-                        <div>
-                            {item.price.toLocaleString('ko-KR')}원
-                        </div>
+                    {/* 설명 */}
+                    <div className='text-secondary mt-2'>
+                        {item.description}
                     </div>
 
-                    <div className='mt-3 fw-semibold fs-5 d-flex justify-content-between align-items-center flex-wrap'>
-                        <div>
-                            수량
-                        </div>
-                        {/* 수량 입력칸 */}
-                        <div className="border rounded-3 d-flex align-items-center">
-                            <span className={`px-3 py-2 ${quantity < 1 ? 'text-secondary' : ''}`} onClick={clickMinus}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-lg" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8" />
-                                </svg>
-                            </span>
-
-                            <input type="text" size={3} pattern="[0-9]*" inputMode="numeric"
-                                ref={inputRef}
-                                className="text-center border-0"
-                                value={quantity === 0 ? '' : quantity}
-                                placeholder='0'
-                                onKeyDown={handleKeyDown}
-                                style={{ caretColor: 'transparent' }}
-                            />
-
-                            <span className={`px-3 py-2 ${quantity >= maxQuantity ? 'text-secondary' : ''}`} onClick={clickPlus}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
-                                </svg>
-                            </span>
-                        </div>
-                    </div>
                 </main>
 
                 {/* 푸터 */}
-                <Footer
-                    value={total <= 0 ? '수량을 입력해주세요.' : total.toLocaleString('ko-KR') + '원 담기'}
-                    onClick={addCart}
-                    disabled={total <= 0 ? true : false}
-                    show={true}
-                />
+                <footer
+                    className='border-top border-success-subtle rounded-4'
+                    style={{
+                        boxShadow: '0 -4px 10px rgba(0, 0, 0, 0.05)'
+                    }}
+                >
+
+                    {/* 수량 */}
+                    <div className='text-center py-4'>
+                        <div className='display-3 fw-semibold text-success'>{quantity.toLocaleString('ko-KR')}개</div>
+                    </div>
+
+                    {/* 넘버패드 */}
+                    <div>
+                        <div className='d-flex gap-2 text-center fw-semibold text-success px-2 mb-1'>
+                            {plusValue.map((value, index) =>
+                                <div key={index} className='w-100 bg-success-subtle rounded-3 py-1'>
+                                    +{value}
+                                </div>
+                            )}
+                        </div>
+
+                        {numberPad.map((row, index) =>
+                            <div key={"row" + index} className='d-flex text-center fw-semibold fs-4'>
+                                {row.map((number, index) =>
+                                    <div
+                                        key={"num" + index}
+                                        className='w-100 py-3 rounded-4'
+                                        style={{ userSelect: 'none', WebkitUserSelect: 'none' }} //텍스트 복사 막기 
+                                        onTouchStart={(e) => e.currentTarget.classList.add('bg-secondary-subtle', 'text-secondary')}
+                                        onTouchEnd={(e) => e.currentTarget.classList.remove('bg-secondary-subtle', 'text-secondary')}
+                                        onTouchCancel={(e) => e.currentTarget.classList.remove('bg-secondary-subtle', 'text-secondary')}
+                                    >
+                                        {number}
+                                    </div>
+                                )}
+                            </div>
+                        )}
+
+                    </div>
+
+                    {/* 장바구니 담기 버튼 */}
+                    <div className="p-2 py-3">
+                        <Button variant="success" disabled={total <= 0} className="w-100 fs-5 fw-semibold p-2 rounded-3" >
+                            {total > 0 ? `${total.toLocaleString('ko-KR')}원 담기` : '수량을 입력해주세요'}
+                        </Button>
+                    </div>
+                </footer>
             </div >
         </MotionPage>
     );
